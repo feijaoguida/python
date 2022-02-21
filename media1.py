3dcb1a645c09f8a5efa28c85a0ced263d1eb84be
Roseweltty <roseweltty@visie.com.br>
2022-02-21 12:01:50 -0300

    media

---
+++ media1.py
@@ -0,0 +1,8 @@
+print('\t ----Cálculo da média aritmética---- ')
+
+nota1 = int(input('Informe a Primeira nota: '))
+nota2 = int(input('Informe a Segunda nota: '))
+
+media = (nota1+nota2)/2
+
+print('Média Arítmetica =', media)
